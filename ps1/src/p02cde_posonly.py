daa14158bea03e4319b6909b2658d3a92be67d30
mzgubic <miha.zgubic@gmail.com>
2018-10-07 14:03:26 +0200

    add ps1-ex2c

--- ps1/src/p02cde_posonly.py
+++ ps1/src/p02cde_posonly.py
@@ -27,7 +27,25 @@ def main(train_path, valid_path, test_path, pred_path):
 
     # *** START CODE HERE ***
     # Part (c): Train and test on true labels
+    x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    theta_0 = np.zeros(shape=(3, ))
+    model = LogisticRegression(theta_0=theta_0)
+    model.fit(x_train, t_train)
+
+    # predict using the trained model
+    x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
+    t_pred = model.predict(x_test)
+
+    # Plot decision boundary on top of validation set set
+    util.plot(x_test, t_test, model.theta, 'output/2c_{ds}.pdf'.format(ds=test_path.split('/')[-1]))
+
+    # Use np.savetxt to save predictions on eval set to pred_path
+    np.savetxt(pred_path, t_pred)
+
     # Part (d): Train on y-labels and test on true labels
     # Part (e): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs
-    # *** END CODER HERE
+    # *** END CODE HERE
+
+
+
