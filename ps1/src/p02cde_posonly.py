2e48ef1ab0153dca5dd1266017514a8c9bcff334
mzgubic <miha.zgubic@gmail.com>
2018-10-07 14:54:03 +0200

    add ps1-ex2e

--- ps1/src/p02cde_posonly.py
+++ ps1/src/p02cde_posonly.py
@@ -25,6 +25,7 @@ def main(train_path, valid_path, test_path, pred_path):
     pred_path_d = pred_path.replace(WILDCARD, 'd')
     pred_path_e = pred_path.replace(WILDCARD, 'e')
 
+    #######################
     # *** START CODE HERE ***
     # Part (c): Train and test on true labels
     x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
@@ -36,12 +37,13 @@ def main(train_path, valid_path, test_path, pred_path):
     x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
     t_pred = model_t.predict(x_test)
 
-    # Plot decision boundary on top of validation set set
+    # Plot decision boundary on top of test set
     util.plot(x_test, t_test, model_t.theta, 'output/2c_{ds}.pdf'.format(ds=test_path.split('/')[-1]))
 
     # Use np.savetxt to save predictions on eval set to pred_path
     np.savetxt(pred_path, t_pred)
 
+    #######################
     # Part (d): Train on y-labels and test on true labels
     x_train, y_train = util.load_dataset(train_path, label_col='y', add_intercept=True)
     model_y = LogisticRegression(theta_0=theta_0)
@@ -51,10 +53,19 @@ def main(train_path, valid_path, test_path, pred_path):
     x_test, y_test = util.load_dataset(test_path, label_col='y', add_intercept=True)
     t_pred = model_t.predict(x_test)
 
-    # Plot decision boundary on top of validation set set
+    # Plot decision boundary on top of test set
     util.plot(x_test, y_test, model_y.theta, 'output/2d_{ds}.pdf'.format(ds=test_path.split('/')[-1]))
 
+    #######################
     # Part (e): Apply correction factor using validation set and test on true labels
+    x_valid, y_valid = util.load_dataset(valid_path, label_col='y', add_intercept=True)
+    pred_y_valid = model_y.predict(x_valid)
+    V_plus_mask = (y_valid == 1)
+    alpha = np.mean(pred_y_valid[V_plus_mask])
+    
+    # Plot decision boundary on top of test set
+    util.plot(x_test, y_test, model_y.theta, 'output/2e_{ds}.pdf'.format(ds=test_path.split('/')[-1]), correction=alpha)
+
     # Plot and use np.savetxt to save outputs
     # *** END CODE HERE
 
